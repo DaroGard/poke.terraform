7abc9ed2d249a8bcd3a3d934e65082512be7150f
DaroGard <alexdg997@gmail.com>
2025-08-03 15:31:20 -0600

    update runtime de python

--- functionapp.tf
+++ functionapp.tf
@@ -5,8 +5,12 @@ resource "azurerm_linux_function_app" "serverless1" {
 
   storage_account_name       = azurerm_storage_account.saccount2.name
   storage_account_access_key = azurerm_storage_account.saccount2.primary_access_key
-  service_plan_id            = azurerm_storage_account.saccount2.id
+  service_plan_id            = azurerm_service_plan.sp.id
 
-  site_config {}
+  site_config {
+    application_stack {
+      python_version = "3.11"
+    }
+  }
 
 }
